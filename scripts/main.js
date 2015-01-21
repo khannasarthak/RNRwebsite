53334e28b994ac9a0dde5a7c067006ec10ba5812
Tapan Ranjan - RNR <tapanranjan@gmail.com>
2015-01-21 22:54:28 +0530

    upload
    -resync

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 var flipper=1;
 
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
 function flip(){
     if(flipper==1)
     {
